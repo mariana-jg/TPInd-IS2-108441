11485df78e414d89f24ccdc8d6fb6f1fb43f0112
Mariana Juarez Goldemberg <mjuarezg@fi.uba.ar>
2025-03-13 20:02:34 -0300

    feat: adding error response struct following RFC 7807 format

---
+++ internal/models/rfc_error.go
@@ -0,0 +1,9 @@
+package models
+
+type RFCError struct {
+	Type     string `json:"type"`
+	Title    string `json:"title"`
+	Status   int    `json:"status"`
+	Detail   string `json:"detail"`
+	Instance string `json:"instance"`
+}

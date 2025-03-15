ae298e12549e7b9153b3d3267e6a125948068323
Mariana Juarez Goldemberg <mjuarezg@fi.uba.ar>
2025-03-15 16:26:40 -0300

    finishing logging

--- internal/handlers/courses_handler.go
+++ internal/handlers/courses_handler.go
@@ -28,7 +28,8 @@ func (h *CourseHandler) DeleteCourseHandler(c *gin.Context) {
 	if error != nil {
 
 		h.Logger.WithFields(logrus.Fields{
-			"method": "GET",
+			"method": "DELETE",
+			"status": http.StatusBadRequest,
 			"path":   c.Request.URL.Path,
 		}).Warn("ID is not a valid number")
 
@@ -48,7 +49,8 @@ func (h *CourseHandler) DeleteCourseHandler(c *gin.Context) {
 		if _, ok := error.(*services.CourseNotFoundError); ok {
 
 			h.Logger.WithFields(logrus.Fields{
-				"method": "GET",
+				"method": "DELETE",
+				"status": http.StatusNotFound,
 				"path":   c.Request.URL.Path,
 				"id":     id,
 			}).Error("Course not found")
@@ -62,6 +64,14 @@ func (h *CourseHandler) DeleteCourseHandler(c *gin.Context) {
 			c.IndentedJSON(http.StatusNotFound, errorResponse)
 			return
 		}
+
+		h.Logger.WithFields(logrus.Fields{
+			"method": "DELETE",
+			"status": http.StatusInternalServerError,
+			"error":  error.Error(),
+			"path":   c.Request.URL.Path,
+		}).Error("Internal Server Error")
+
 		errorResponse := models.NewRFCError(
 			http.StatusInternalServerError,
 			"Internal server error",
@@ -71,6 +81,12 @@ func (h *CourseHandler) DeleteCourseHandler(c *gin.Context) {
 		c.IndentedJSON(http.StatusInternalServerError, errorResponse)
 		return
 	}
+
+	h.Logger.WithFields(logrus.Fields{
+		"method": "DELETE",
+		"status": http.StatusNoContent,
+		"path":   c.Request.URL.Path,
+	}).Warn("No Content response")
 
 	c.IndentedJSON(http.StatusNoContent, nil)
 
@@ -83,6 +99,7 @@ func (h *CourseHandler) GetCourseHandler(c *gin.Context) {
 
 		h.Logger.WithFields(logrus.Fields{
 			"method": "GET",
+			"status": http.StatusBadRequest,
 			"path":   c.Request.URL.Path,
 		}).Warn("ID is not a valid number")
 
@@ -102,6 +119,7 @@ func (h *CourseHandler) GetCourseHandler(c *gin.Context) {
 
 			h.Logger.WithFields(logrus.Fields{
 				"method": "GET",
+				"status": http.StatusNotFound,
 				"path":   c.Request.URL.Path,
 				"id":     id,
 			}).Error("Course not found")
@@ -116,45 +134,13 @@ func (h *CourseHandler) GetCourseHandler(c *gin.Context) {
 			return
 		}
 
-		errorResponse := models.NewRFCError(
-			http.StatusInternalServerError,
-			"Internal server error",
-			error.Error(),
-			c.Request.URL.Path,
-		)
-		c.IndentedJSON(http.StatusInternalServerError, errorResponse)
-		return
-	}
+		h.Logger.WithFields(logrus.Fields{
+			"method": "GET",
+			"status": http.StatusInternalServerError,
+			"error":  error.Error(),
+			"path":   c.Request.URL.Path,
+		}).Error("Internal Server Error")
 
-	c.IndentedJSON(http.StatusOK, gin.H{
-		"data": course,
-	})
-
-}
-
-func (h *CourseHandler) GetCoursesHandler(c *gin.Context) {
-	courses := h.CourseService.GetCourses()
-	c.IndentedJSON(http.StatusOK, gin.H{
-		"data": courses,
-	})
-}
-
-func (h *CourseHandler) CreateCourseHandler(c *gin.Context) {
-	var course models.Course
-
-	if error := c.ShouldBindJSON(&course); error != nil {
-		errorResponse := models.NewRFCError(
-			http.StatusBadRequest,
-			"Bad request error",
-			"Error on JSON structure",
-			c.Request.URL.Path,
-		)
-		c.IndentedJSON(http.StatusBadRequest, errorResponse)
-		return
-	}
-
-	createdCourse, error := h.CourseService.CreateCourse(course)
-	if error != nil {
 		errorResponse := models.NewRFCError(
 			http.StatusInternalServerError,
 			"Internal server error",
@@ -167,6 +153,75 @@ func (h *CourseHandler) CreateCourseHandler(c *gin.Context) {
 
 	h.Logger.WithFields(logrus.Fields{
 		"method": "GET",
+		"status": http.StatusOK,
+		"path":   c.Request.URL.Path,
+	}).Info("Request successful")
+
+	c.IndentedJSON(http.StatusOK, gin.H{
+		"data": course,
+	})
+
+}
+
+func (h *CourseHandler) GetCoursesHandler(c *gin.Context) {
+	courses := h.CourseService.GetCourses()
+
+	h.Logger.WithFields(logrus.Fields{
+		"method": "GET",
+		"status": http.StatusOK,
+		"path":   c.Request.URL.Path,
+	}).Info("Request successful")
+
+	c.IndentedJSON(http.StatusOK, gin.H{
+		"data": courses,
+	})
+}
+
+func (h *CourseHandler) CreateCourseHandler(c *gin.Context) {
+	var course models.Course
+
+	if error := c.ShouldBindJSON(&course); error != nil {
+
+		h.Logger.WithFields(logrus.Fields{
+			"method": "POST",
+			"status": http.StatusBadRequest,
+			"error":  error.Error(),
+			"path":   c.Request.URL.Path,
+		}).Warn("Bad Request received")
+
+		errorResponse := models.NewRFCError(
+			http.StatusBadRequest,
+			"Bad request error",
+			"Error on JSON structure",
+			c.Request.URL.Path,
+		)
+		c.IndentedJSON(http.StatusBadRequest, errorResponse)
+		return
+	}
+
+	createdCourse, error := h.CourseService.CreateCourse(course)
+	if error != nil {
+
+		h.Logger.WithFields(logrus.Fields{
+			"method": "POST",
+			"status": http.StatusInternalServerError,
+			"error":  error.Error(),
+			"path":   c.Request.URL.Path,
+		}).Error("Internal Server Error")
+
+		errorResponse := models.NewRFCError(
+			http.StatusInternalServerError,
+			"Internal server error",
+			error.Error(),
+			c.Request.URL.Path,
+		)
+		c.IndentedJSON(http.StatusInternalServerError, errorResponse)
+		return
+	}
+
+	h.Logger.WithFields(logrus.Fields{
+		"method": "POST",
+		"status": http.StatusCreated,
 		"path":   c.Request.URL.Path,
 	}).Info("Course created successfully")
 

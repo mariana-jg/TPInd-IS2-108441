6d486aec0b217b3ea2d59de5b1813137d3f1b559
Mariana Juarez Goldemberg <mjuarezg@fi.uba.ar>
2025-03-16 11:29:57 -0300

    feat: adding challenge restriction on description length

--- internal/models/course.go
+++ internal/models/course.go
@@ -3,5 +3,5 @@ package models
 type Course struct {
 	ID          int    `json:"id"`
 	Title       string `json:"title" binding:"required"`
-	Description string `json:"description" binding:"required"`
+	Description string `json:"description" binding:"required,min=50,max=255"`
 }
--- internal/repositories/migrations.go
+++ internal/repositories/migrations.go
@@ -12,11 +12,11 @@ func RunMigrations(db *pgxpool.Pool) error {
         CREATE TABLE IF NOT EXISTS courses (
             id SERIAL PRIMARY KEY,
             title VARCHAR(255) NOT NULL,
-            description TEXT NOT NULL
+            description VARCHAR(255) NOT NULL
         );
     `)
 	if err != nil {
-		return fmt.Errorf("failed to run migrations: %w", err)
+		return fmt.Errorf("failed to run migrations to create or load the table: %w", err)
 	}
 
 	return nil

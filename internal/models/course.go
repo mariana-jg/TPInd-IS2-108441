db23d61b941baee13057c26937a9b088c7e72d8e
Mariana Juarez Goldemberg <mjuarezg@fi.uba.ar>
2025-03-17 20:21:01 -0300

    test: adding models doc

--- internal/models/course.go
+++ internal/models/course.go
@@ -1,5 +1,7 @@
+// Package models has the structs that are used to represent the data in the application.
 package models
 
+// Course is the struct that represents a course in the application
 type Course struct {
 	ID          int    `json:"id"`
 	Title       string `json:"title" binding:"required"`
